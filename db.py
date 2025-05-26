425ac9ebd944f3b13de0e488dfbcf0c2079760b9
Ishita-Si <lilyslifespiffing09@gmail.com>
2025-05-27 00:22:56 +0530

    Refactor db.py

--- db.py
+++ db.py
@@ -2,13 +2,14 @@ import mysql.connector
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv()  # Load variables from .env
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("127.0.0.1"),
-        user=os.getenv("Database"),
-        password=os.getenv("Blackpink09"),
-        database=os.getenv("clinic_db")
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
+
 
